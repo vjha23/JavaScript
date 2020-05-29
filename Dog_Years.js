3555d923f022a4b74dc776f42594927be2601063
Vijay Jha <51393417+vjha23@users.noreply.github.com>
2020-05-29 20:18:08 +0530

    Add files via upload

---
+++ Dog_Years.js
@@ -0,0 +1,17 @@
+// creating a variable for my age
+var myAge=24;
+// creating early age variable
+var earlyYears=2;
+earlyYears*=10.5;
+// subtracting 2 from current age
+var laterYears=myAge-2;
+// multiply 4 for future age
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+// adding the dog age
+var myAgeInDogYears=earlyYears+laterYears;
+// writing my name
+var myName='Vijay'.toLowerCase();
+// displaying description about me and mydog age
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
---
+++ Kelvin_Weather.js
@@ -0,0 +1,15 @@
+// created a constant variable Kelvin and assign vallue to it
+const Kelvin=0;
+// created another const variable celsius and asiign value to it
+const Celsius=Kelvin-273;
+
+// create an var type variable fahrenhite it can mutable
+var fahrenheit=Celsius*(9/5)+32;
+// converting decimal to whole no
+fahrenheit=Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+var Newton=Celsius * (33/100);
+Newton=Math.floor(Newton);
+console.log(Newton);
